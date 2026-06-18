0d6e6eb4e8e8fe6b39369b17e0534769a7b258bb
cezooinminutes <cezooinminutes@gmail.com>
2026-06-18 12:41:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+require("dotenv").config();
 
+const express = require("express");
+const app = express();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Cezo Backend Running",
+    projectId: firebaseConfig.projectId
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
